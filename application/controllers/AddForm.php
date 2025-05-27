69317021ccaa1f7c852a1862b5bd0cf9734ed09a
ryosuke0828 <ryosukee.zzz@gmail.com>
2025-05-27 18:25:34 +0900

    追加formのviewの作成

---
+++ application/controllers/AddForm.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AddForm extends CI_Controller {
+
+	public function AddForm()
+	{
+		$this->load->view('welcome_message');
+	}
+}
